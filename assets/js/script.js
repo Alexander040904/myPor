86ca5724989937fe3d5dc373c37808d33929dcb3
jona <jonathanalexanderramirezortega@gmail.com>
2025-11-24 00:42:55 -0600

    ✨ feat(Trayectoria, Navigation): Index, script Se actualizo la informacion de mi experiencia Se elimino la seccion de porcentajes Se arreglo una correccion para aceptar en mayusculas y minuscala la navegacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -148,7 +148,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
     for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+      if (this.innerHTML.toUpperCase() === pages[i].dataset.page.toUpperCase()) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
